60d7d4e2d2d352a91a4d10e06de1011957ca71ce
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-10 11:59:11 -0400

    Opening the Map Modal

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -15,7 +15,9 @@ export class LocationPickerComponent implements OnInit {
   }
 
   onPickLocation() {
-    this.modalCtrl.create({ component: MapModalComponent });
+    this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
+      modalEl.present();
+    });
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
 import { MapModalComponent } from './map-modal/map-modal.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
     declarations: [LocationPickerComponent, MapModalComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, IonicModule],
     exports: [LocationPickerComponent, MapModalComponent],
     entryComponents: [MapModalComponent]
 })
