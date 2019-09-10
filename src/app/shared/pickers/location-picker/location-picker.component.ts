5c5c7476e9eaa6dd460521db977bcd41347283f3
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-10 15:42:01 -0400

    Fetching a static image URL

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { environment } from '../../../../environments/environment';
-
+import { PlaceLocation } from '../../../places/location.model';
 
 @Component({
   selector: 'app-location-picker',
@@ -23,8 +24,22 @@ export class LocationPickerComponent implements OnInit {
         if (!modalData.data) {
           return;
         }
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe((address) => {
-          console.log(address);
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageUrl: null
+        };
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+          switchMap(address => {
+            pickedLocation.address = address;
+            return of(
+              this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
+            );
+          })
+        ).subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUrl = staticMapImageUrl;
+          
         });
       });
       modalEl.present();
@@ -33,13 +48,22 @@ export class LocationPickerComponent implements OnInit {
 
   private getAddress(lat: number, lng: number) {
     // tslint:disable-next-line: max-line-length
-    return this.http.get<any>(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIKey}`
-    ).pipe(map(geoData => {
-      if (!geoData || !geoData.results || geoData.results.length === 0) {
-        return null;
-      }
-      return geoData.results[0].formatted_address;
-    }));
+    return this.http
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIKey}`
+      )
+      .pipe(
+        map(geoData => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+          return geoData.results[0].formatted_address;
+        })
+      );
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap&
+    markers=color:red%7Clabel:Place%7C${lat},${lng}&key=${environment.googleMapsAPIKey}`;
   }
 }
