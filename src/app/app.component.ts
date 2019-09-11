b64bd1181c3b183e21e0dd2284abb2690139e7e3
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-10 21:28:27 -0400

    Using capacitor plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Plugins, Capacitor } from '@capacitor/core';
 import { AuthService } from './auth/auth.service';
 
 @Component({
@@ -14,8 +12,6 @@ import { AuthService } from './auth/auth.service';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
     private authService: AuthService,
     private router: Router
   ) {
@@ -23,9 +19,11 @@ export class AppComponent {
   }
 
   initializeApp() {
+    // console.log(this.platform.is('hybrid'));
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (Capacitor.isPluginAvailable('SplashScreen')) {
+        Plugins.SplashScreen.hide();
+      }
     });
   }
 
