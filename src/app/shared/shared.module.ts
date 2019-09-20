4a822be24c8eb43db55d973c94e09f6c116db888
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-20 15:12:19 -0400

    starting with the image picker

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LocationPickerComponent } from './pickers/location-picker/location-picker.component';
 import { MapModalComponent } from './map-modal/map-modal.component';
 import { IonicModule } from '@ionic/angular';
+import { ImagePickerComponent } from './pickers/image-picker/image-picker.component';
 
 
 @NgModule({
-    declarations: [LocationPickerComponent, MapModalComponent],
+    declarations: [LocationPickerComponent, MapModalComponent, ImagePickerComponent],
     imports: [CommonModule, IonicModule],
     exports: [LocationPickerComponent, MapModalComponent],
     entryComponents: [MapModalComponent]
