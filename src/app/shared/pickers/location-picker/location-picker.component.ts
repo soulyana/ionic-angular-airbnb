c8813a0be9a6287da383fe4751c53900dfa687ef
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-20 14:51:42 -0400

    getting the user location

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ActionSheetController, AlertController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { Plugins, Capacitor } from '@capacitor/core';
 
 import { MapModalComponent } from '../../map-modal/map-modal.component';
 import { environment } from '../../../../environments/environment';
-import { PlaceLocation } from '../../../places/location.model';
+import { PlaceLocation, Coordinates } from '../../../places/location.model';
 
 @Component({
   selector: 'app-location-picker',
@@ -18,11 +19,55 @@ export class LocationPickerComponent implements OnInit {
   selectedLocationImage: string;
   isLoading = false;
 
-  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
+  constructor(
+    private modalCtrl: ModalController,
+    private http: HttpClient,
+    private actionSheetCtrl: ActionSheetController,
+    private alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {}
 
   onPickLocation() {
+    this.actionSheetCtrl.create({
+      header: 'Please Choose',
+      buttons: [
+        { text: 'Auto-Locate', handler: () => {
+          this.locateUser();
+        } },
+        { text: 'Pick on Map', handler: () => {
+          this.openMap();
+        } },
+        { text: 'Cancel', role: 'cancel' }
+      ]
+    }).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+  }
+
+  private locateUser() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      this.showErrorAlert();
+      return;
+    }
+    Plugins.Geolocation.getCurrentPosition().then(geoPosition => {
+      const coordinates: Coordinates = {
+        lat: geoPosition.coords.latitude,
+        lng: geoPosition.coords.longitude
+      };
+    }).catch(err => {
+      this.showErrorAlert();
+    });
+  }
+
+  private showErrorAlert() {
+    this.alertCtrl.create({
+      header: 'Could not fetch location', 
+      message: 'Please use the map to pick a location!'
+    });
+  }
+
+  private openMap() {
     this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
       modalEl.onDidDismiss().then(modalData => {
         if (!modalData.data) {
@@ -35,24 +80,25 @@ export class LocationPickerComponent implements OnInit {
           staticMapImageUrl: null
         };
         this.isLoading = true;
-        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
-          switchMap(address => {
-            pickedLocation.address = address;
-            return of(
-              this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
-            );
-          })
-        ).subscribe(staticMapImageUrl => {
-          pickedLocation.staticMapImageUrl = staticMapImageUrl;
-          this.selectedLocationImage = staticMapImageUrl;
-          this.isLoading = false;
-          this.locationPick.emit(pickedLocation);
-        });
+        this.getAddress(modalData.data.lat, modalData.data.lng)
+          .pipe(
+            switchMap(address => {
+              pickedLocation.address = address;
+              return of(
+                this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14)
+              );
+            })
+          )
+          .subscribe(staticMapImageUrl => {
+            pickedLocation.staticMapImageUrl = staticMapImageUrl;
+            this.selectedLocationImage = staticMapImageUrl;
+            this.isLoading = false;
+            this.locationPick.emit(pickedLocation);
+          });
       });
       modalEl.present();
     });
   }
-
   private getAddress(lat: number, lng: number) {
     // tslint:disable-next-line: max-line-length
     return this.http
